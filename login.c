ea252172c35fb0c058b7dbe706d8198b09739fad
matthew <matthew@qq.com>
2021-01-30 18:08:48 -0800

    first commit

---
+++ login.c
@@ -0,0 +1,17 @@
+#define USER_NAME_LEN 50
+#define PASSWORD_LEN 50
+class Login{
+public:
+	Login();
+	~Login();
+private:
+	init_sdk();
+private:
+string item_name;
+string password;
+
+}
+
+
+int main(){
+}
